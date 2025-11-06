e3e0960d0506786061d99ad96ca9d748754c063f
lglducci <lglducci@hotmail.com>
2025-11-05 22:36:25 -0300

    comit

--- Banco de dados/Backup-Producao/empresas_rows.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "public"."empresas" ("id_empresa", "nome", "cnpj", "created_at", "ie", "saudacao") VALUES ('1', 'La Cantinha', '53.803.841/0001-01', '2025-09-30 21:22:42.1198', '600.132.222.111', ' Olá, seja bem-vindo(a) ao atendimento digital da La Cantina.
-
- Nosso horário de atendimento é de TERÇA a DOMINGO das 18:00h às 24:00h.
- 
- Olá, tudo bem? Já estou enviando o Menu, aguarde... '), ('2', 'Parmezzoni Pizzaria', '32.180.369/0001-10', '2025-10-03 08:10:03.366686', '349.043.921.112', '');
